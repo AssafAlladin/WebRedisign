be9fb3fec47366c4cdeb926fbf2ed74031abe28c
AssafAlladin <62299796+AssafAlladin@users.noreply.github.com>
2020-05-05 17:53:09 -0500

    updtae

--- Redisign/js/A_app.js
+++ Redisign/js/A_app.js
@@ -1,7 +1,9 @@
 $(function(){
     let dropdown = $(".button");
     let menu = $("nav .outside li:nth-child(2)");
+    let menu2 = $("header .hidden .outside li:nth-child(2)");
     let secondary = $("nav .outside li .inside");
+    let secondary2 = $("header .hidden .outside li .inside");
     let bottomNav = $("footer .nav2 li:nth-child(2)");
 
     $(dropdown).on("click", function(){
@@ -9,6 +11,9 @@ $(function(){
     });
     $(menu).on("click", function() {
         $(secondary).slideToggle();
+    });
+    $(menu2).on("click", function() {
+        $(secondary2).slideToggle();
     });
     $(bottomNav).on("click", function() {
         $(".bottom").slideToggle();
