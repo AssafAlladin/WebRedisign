8165a5c5b939f836a471e709202a0e01a43b0acd
AssafAlladin <62299796+AssafAlladin@users.noreply.github.com>
2020-05-06 11:24:56 -0500

    Updated Nav

--- Redisign/js/A_app.js
+++ Redisign/js/A_app.js
@@ -1,21 +1,15 @@
-$(function(){
-    let dropdown = $(".button");
-    let menu = $("nav .outside li:nth-child(2)");
-    let menu2 = $("header .hidden .outside li:nth-child(2)");
-    let secondary = $("nav .outside li .inside");
-    let secondary2 = $("header .hidden .outside li .inside");
-    let bottomNav = $("footer .nav2 li:nth-child(2)");
+$(function() {
+    let menuToggler = $("nav button.navbar-toggler");
+    let dropdownToggler = $("a.nav-link.dropdown-toggle")
 
-    $(dropdown).on("click", function(){
-        $("nav").slideToggle();
+    menuToggler.on("click", function(e) {
+        e.preventDefault
+        let navigation = $(this).attr("data-target");
+        $(navigation).toggleClass("open");
     });
-    $(menu).on("click", function() {
-        $(secondary).slideToggle();
-    });
-    $(menu2).on("click", function() {
-        $(secondary2).slideToggle();
-    });
-    $(bottomNav).on("click", function() {
-        $(".bottom").slideToggle();
+
+    dropdownToggler.on("click", function(e) {
+        e.preventDefault();
+        $(this).siblings().toggleClass("open");
     });
 });
